0581f719f25e2a77deef22ba872eee3b4eba359a
Elnaz Fe <elnaz.farrokhi1988@gmail.com>
2023-06-01 14:54:59 +0100

    pages

---
+++ src/Pages/JobListPage.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+ 
+const API_URL = "http://localhost:5005";
+
+function JobListPage() {
+    const [Jobs, setJobs] = useState([]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JobListPage
